ed569cf46a1818abd7f39ebac761ebc44ed5eda5
Julien Pollart <jpollart@gmail.com>
2022-12-21 23:22:29 +0100

    Add a customer_test.py file (unit test for the customer class)

---
+++ tests/test_customer.py
@@ -0,0 +1,44 @@
+import unittest
+from main_project.classes.customer import Customer
+
+class TestCustomer(unittest.TestCase):
+    def test_get_customer(self):
+        # Test avec un id valide
+        id_customer = 1
+        customer = Customer.get_customer(id_customer)
+        self.assertIsNotNone(customer)
+
+        # Test avec un id non valide
+        id_customer = -1
+        customer = Customer.get_customer(id_customer)
+        self.assertIsNone(customer)
+
+    def test_insert_db(self):
+        # Test d'insertion d'un nouveau client valide
+        customer = Customer()
+        customer.first_name = "John"
+        customer.last_name = "Doe"
+        customer.phone = 1234567890
+        customer.mail = "john.doe@example.com"
+        customer.address = "123 Main Street"
+        self.assertTrue(customer.insert_db())
+
+        # Test d'insertion d'un nouveau client avec des valeurs non valides
+        customer = Customer()
+        customer.first_name = ""
+        customer.last_name = ""
+        customer.phone = 1234567890
+        customer.mail = "invalid.email@"
+        customer.address = "123 Main Street"
+        self.assertFalse(customer.insert_db())
+
+    def test_name_table(self):
+        # Test de récupération du nom de la table client
+        self.assertEqual(Customer.name_table(), "customer")
+
+    def test_id_column(self):
+        # Test de récupération du nom de la colonne id de la table client
+        self.assertEqual(Customer.id_column(), "id")
+
+if __name__ == '__main__':
+    unittest.main()
